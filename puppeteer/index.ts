ebb5bcaaa341c315ab5c3a50680adb80490500a0
Tetsuya Yoshida <yoshida@niiyz.com>
2021-07-17 11:51:46 +0900

    typescript
    docker-compose exec puppeteer ts-node index.ts

--- puppeteer/index.ts
+++ puppeteer/index.ts
@@ -1,9 +1,7 @@
-console.log('puppeteer')
+import puppeteer from "puppeteer";
 
-const pptr = require('puppeteer');
-
-async function run() {
-    const browser = await pptr.launch({
+const main = async (): Promise<void> => {
+    const browser = await puppeteer.launch({
         headless: true,
         args: [
             '--no-sandbox',
@@ -11,21 +9,19 @@ async function run() {
             '--disable-dev-shm-usage'
         ]
     })
-    // create page
     const page = await browser.newPage();
 
-    // fix screen size
     await page.setViewport({ width: 720, height: 600 })
 
-    // select url
-    await page.goto('https://www.google.co.jp/');
+    await page.goto('https://www.openstreetmap.org/');
 
     console.log(await page.title());
 
     await page.screenshot({path: "/app/test.png", fullPage: true});
 
-    // close browser
     await browser.close()
 }
 
-run();
+main().then(() => {
+    console.log("puppeteer finish");
+});
